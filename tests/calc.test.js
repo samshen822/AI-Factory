b168f270ec0ab56479ed3ed6e2222dae3e7f6699
Sam <278046570+samshen822@users.noreply.github.com>
2026-04-21 15:46:05 +0800

    experiment: iter2 multiply tests

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require('../src/calc');
+const { add, subtract, multiply } = require('../src/calc');
 
 test('add 1 + 2 = 3', () => {
   expect(add(1, 2)).toBe(3);
@@ -10,4 +10,12 @@ test('subtract 5 - 3 = 2', () => {
 
 test('subtract handles negatives', () => {
   expect(subtract(-2, -5)).toBe(3);
+});
+
+test('multiply 4 * 3 = 12', () => {
+  expect(multiply(4, 3)).toBe(12);
+});
+
+test('multiply by zero', () => {
+  expect(multiply(99, 0)).toBe(0);
 });
