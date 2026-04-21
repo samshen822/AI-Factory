741346029c817cba468fa35c0150a80e18e984dc
Sam <278046570+samshen822@users.noreply.github.com>
2026-04-21 15:45:23 +0800

    experiment: iter1 subtract tests

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,5 +1,13 @@
-const { add } = require('../src/calc');
+const { add, subtract } = require('../src/calc');
 
 test('add 1 + 2 = 3', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('subtract 5 - 3 = 2', () => {
+  expect(subtract(5, 3)).toBe(2);
+});
+
+test('subtract handles negatives', () => {
+  expect(subtract(-2, -5)).toBe(3);
 });
